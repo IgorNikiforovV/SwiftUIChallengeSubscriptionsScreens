da1fcee3adbcd002811a1232e9250a4801afcb10
Igor Nikiforov <ivnikiforo@mts.ru>
2023-03-17 11:56:39 +0600

    Initial Commit

---
+++ SwiftUIStacks/SwiftUIStacksApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUIStacksApp.swift
+//  SwiftUIStacks
+//
+//  Created by Никифоров Игорь Викторович on 17.03.2023.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUIStacksApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
